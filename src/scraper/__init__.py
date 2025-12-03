6444b91e831c5eb117d5dbff9199c36ea07df737
FerranGuardia <onlyartferran@gmail.com>
2025-12-03 10:35:33 +0100

    feat(scraper): export new ChapterUrlFetcher and ContentScraper classes

--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -2,3 +2,16 @@
 Scraper module - Content extraction from web sources.
 """
 
+from .generic_scraper import GenericScraper
+from .base_scraper import BaseScraper
+from .url_fetcher import ChapterUrlFetcher
+from .content_scraper import ContentScraper
+
+__all__ = [
+    "GenericScraper",
+    "BaseScraper",
+    "ChapterUrlFetcher",
+    "ContentScraper",
+]
+
+
